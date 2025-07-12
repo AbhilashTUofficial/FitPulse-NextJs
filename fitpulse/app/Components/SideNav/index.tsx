36b6e40e228270ab4feaf5d689bf2ba3cfa3d0e1
AbhilashTUofficial <60460499+AbhilashTUofficial@users.noreply.github.com>
2025-07-12 11:02:46 +0530

    update

--- fitpulse/app/Components/SideNav/index.tsx
+++ fitpulse/app/Components/SideNav/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import './style.css';
 
 export default function NavBar() {
     return (
-        <div className="navBarCont flex flex-col h-screen w-20 bg-red-600 sticky top-0  justify-between">
+        <div className="navBarCont flex flex-col h-screen w-20 sticky top-0  justify-between">
             <div className="logo flex items-center justify-center">
                 Logo
             </div>
-            <div className="btnCont flex flex-col items-center justify-center">
-                <div className="btn1">1</div>
-                <div className="btn1">2</div>
-                <div className="btn1">3</div>
-                <div className="btn1">4</div>
-                <div className="btn1">5</div>
+            <div className="btnCont flex flex-col h-2/4 items-center justify-around">
+                <div className="btn1 group btn flex justify-center items-center relative overflow-visible">
+                    <div className="btnContent absolute z-10">1</div>
+
+                    <div className="btnHover absolute top-0 left-full h-full w-0 bg-white transition-all duration-300 group-hover:w-[60px] rounded-r-full z-0"></div>
+                </div>
+
+                <div className="btn1 btn flex justify-center items-center">2</div>
+                <div className="btn1 btn flex justify-center items-center">3</div>
+                <div className="btn1 btn flex justify-center items-center">4</div>
+                <div className="btn1 btn flex justify-center items-center">5</div>
+                <div className="btn1 btn flex justify-center items-center">6</div>
             </div>
-            <div className="logOutBtn flex items-center justify-center">Logout</div>
+
+            <div className="logOutBtn flex items-center justify-center">L.Out</div>
         </div>
     );
 }
